7bc563a4249159bf358dbd0cbac0671a6d0f97a5
GavinGe3 <gavin.ge23@ycdsbk12.ca>
2023-01-21 14:55:24 -0500

    Added Datapoint Class and CSV Files.

---
+++ src/Data/CountryDataPoint.java
@@ -0,0 +1,19 @@
+package Data;
+
+public class CountryDataPoint {
+    private int year;
+    private double budget;
+    private double personnel;
+    private double percentageOfGDP;
+    private double EquipmentPercent;
+    private double PersonnelPercent;
+    private double InfrastructurePercent;
+    private double OtherPercent;
+
+    public int addData;
+
+    public CountryDataPoint() {
+        addData = 0;
+    }
+    
+}
