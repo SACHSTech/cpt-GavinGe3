43f4478bf41217fdaa6527f57cb1685ba065b72d
GavinGe3 <gavin.ge23@ycdsbk12.ca>
2023-01-21 22:05:05 -0500

    Added add method to DataPoint

--- src/Data/CountryDataPoint.java
+++ src/Data/CountryDataPoint.java
@@ -1,19 +1,38 @@
 package Data;
 
 public class CountryDataPoint {
+    private String Country;
     private int year;
     private double budget;
     private double personnel;
-    private double percentageOfGDP;
-    private double EquipmentPercent;
-    private double PersonnelPercent;
-    private double InfrastructurePercent;
-    private double OtherPercent;
-
+    private double GDP;
     public int addData;
 
     public CountryDataPoint() {
         addData = 0;
     }
-    
+
+    public void add(String value){
+        switch(addData){
+            case 0:
+            this.Country = value;
+            break;
+            case 1:
+            this.year = Integer.parseInt(value);
+            break;
+            case 2:
+            this.budget = Double.parseDouble(value);
+            break;
+            case 3:
+            this.personnel = Double.parseDouble(value);
+            break;
+            case 4:
+            this.GDP = Double.parseDouble(value);
+        }
+        this.addData++;
+    }
+    public String getCountry() {
+        return this.Country;
+    }
+
 }
