d272d1c1fc3ca3b3251e276c5684da96f2cd8753
GavinGe3 <gavin.ge23@ycdsbk12.ca>
2023-01-22 12:12:00 -0500

    Added main function to test CSVscraper.

---
+++ src/Data/Main.java
@@ -0,0 +1,40 @@
+package Data;
+
+import java.io.FileNotFoundException;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+    public void start(Stage primaryStage) {
+
+        
+
+
+
+
+    }
+
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Data newData = new Data();
+
+
+       for (int i = 0; i < 240; i++) {
+        System.out.println(newData.getData(i).getCountry());
+       }
+       
+
+        
+
+    
+
+    }
+
+}
