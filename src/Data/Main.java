7e29819573d53fb17f5a945a32a52151876d0970
GavinGe3 <gavin.ge23@ycdsbk12.ca>
2023-01-22 22:14:16 -0500

    Created Chart.

--- src/Data/Main.java
+++ src/Data/Main.java
@@ -10,31 +10,33 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import charts.ChartGenerator;
 public class Main extends Application{
+
     public void start(Stage primaryStage) {
 
-        
+        ChartGenerator newChart = new ChartGenerator();
+        newChart.updateLineChart();
 
+        Scene scene = new Scene(newChart.getChart(), 1000,1000);
 
-
-
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
-
+    public void change(ActionEvent event){
+        if (newChart.)
+    }
 
     public static void main(String[] args) throws FileNotFoundException {
-        Data newData = new Data();
 
+        launch(args);
 
-       for (int i = 0; i < 240; i++) {
-        System.out.println(newData.getData(i).getCountry());
+        DataScrape newData = new DataScrape();
+
+       for (int i = 0; i < 248; i++) {
+
+            System.out.println((newData.getData().get(i)).getGDP());
        }
-       
-
-        
-
-    
-
     }
-
 }
