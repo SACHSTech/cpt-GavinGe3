c6f901f10878b57140adc409e0567a50f1f7e41f
GavinGe3 <gavin.ge23@ycdsbk12.ca>
2023-01-24 00:06:53 -0500

    Added Bar Chart Generator.

--- src/charts/ChartGenerator.java
+++ src/charts/ChartGenerator.java
@@ -9,7 +9,9 @@ import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.Chart;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
@@ -21,6 +23,7 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.util.converter.NumberStringConverter;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -45,10 +48,15 @@ public class ChartGenerator  {
     private boolean showBudget;
     private boolean showPersonnel;
     private DataScrape scraper;
-    private VBox chart;
+    private HBox chart;
+    private HBox barChart;
     private ScrollPane boxPane;
     private VBox checkBoxes;
+    private VBox barCheckBoxes;
+    private ScrollPane barScrollPane;
+
     private List<CheckBox> countryBoxes = new ArrayList<>();
+    private List<CheckBox> barCountryBoxes = new ArrayList<>();
 
 
     public ChartGenerator() {
@@ -56,78 +64,65 @@ public class ChartGenerator  {
         this.showBudget = false;
         this.showPersonnel = false;
         this.scraper = new DataScrape();
-        this.chart = new VBox();
+        this.chart = new HBox();
         for (int i = 0; i < 31; i++){
             CheckBox checkbox = new CheckBox(scraper.getData().get(i*8).getCountry());
             checkbox.setSelected(false);
             countryBoxes.add(checkbox);
         }
+        for (int i = 0; i < 31; i ++){
+            CheckBox checkbox = new CheckBox(scraper.getData().get(i*8).getCountry());
+            checkbox.setSelected(false);
+            barCountryBoxes.add(checkbox);
+        }
         this.boxPane = new ScrollPane();
         this.checkBoxes = new VBox();
+        this.barChart = new HBox();
+        this.barScrollPane = new ScrollPane();
+        this.barCheckBoxes = new VBox();
 
 
     }
 
+    public void updateBarChart(){
+
+        List<XYChart.Series<String,Number>> newSeries = new ArrayList<>();
+        List<XYChart.Series<Number,Number>> holder = new ArrayList<>();
+
+        CategoryAxis xAxis = new CategoryAxis();
+        xAxis.setLabel("Years 2014 to 2021");
+        NumberAxis yAxis = new NumberAxis();
+        yAxis.setLabel("Military Budget in Years");
+        BarChart<String, Number> barchart = new BarChart<>(xAxis, yAxis);
+
+        barChart.setPrefHeight(600);
+        barChart.setPrefWidth(800);
+
+        ChoiceBox<String> dataTypeBar = new ChoiceBox<String>(FXCollections.observableArrayList("Personnel", "GDP", "Military Spending"));
+
+        addDataSeries(dataTypeBar, barchart, holder, newSeries, barCountryBoxes, 2);
+
+        this.barChart.getChildren().addAll(barchart, dataTypeBar);
+        this.barCheckBoxes.getChildren().addAll(barCountryBoxes);
+        this.barScrollPane.setContent(barCheckBoxes);
+        this.barChart.getChildren().addAll(barScrollPane);
+
+    }
     
     public void updateLineChart(){
 
-        ChoiceBox<String> dataType = new ChoiceBox<String>(FXCollections.observableArrayList("Personnel", "GDP", "Military Spending"));
-        
         List<XYChart.Series<Number, Number>> CurrentSeriesList = new ArrayList<>();
+        List<XYChart.Series<String, Number>> holder = new ArrayList<>();
+
         NumberAxis xAxis = new NumberAxis("Year",2014,2021,1);
         NumberAxis yAxis = new NumberAxis();
         LineChart<Number,Number> lineChart = new LineChart<Number,Number>(xAxis,yAxis);
+        lineChart.setPrefHeight(600);
+        lineChart.setPrefWidth(800);
 
-        dataType.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue ) -> {
-            if(newValue.equals("Personnel")){
-                lineChart.setTitle("Personnel");
-                lineChart.getData().clear();
-                CurrentSeriesList.clear();
+        ChoiceBox<String> dataType = new ChoiceBox<String>(FXCollections.observableArrayList("Personnel", "GDP", "Military Spending"));
 
-                for (int i = 0; i < 31; i++){
-                    countryBoxes.get(i).setSelected(false);
-
-                    CurrentSeriesList.add(this.personnelSeries().get(i));
-                }
-            }
-            else if (newValue.equals("GDP")){
-                lineChart.setTitle("GDP");
-                lineChart.getData().clear();
-                CurrentSeriesList.clear();
-
-                for (int i = 0; i < 31; i++){
-                    countryBoxes.get(i).setSelected(false);
-                    CurrentSeriesList.add(this.GDPseries().get(i));
-                }
-            }
-            else if (newValue.equals("Military Spending")){
-                
-                lineChart.setTitle("Military Spending");
-
-                lineChart.getData().clear();
-                CurrentSeriesList.clear();
-
-                for (int i = 0; i < 31; i++){
-                    countryBoxes.get(i).setSelected(false);
-                    CurrentSeriesList.add(this.budgetSeries().get(i));
-                }
-             
-            }
-        });
-
-        for (int i = 0; i < 31; i++){
-            final int index = i; 
-            EventHandler<ActionEvent> checkBoxEventHandler = 
-            e -> {
-                if(countryBoxes.get(index).isSelected()){
-                    lineChart.getData().add(CurrentSeriesList.get(index));
-                }
-                else{
-                    lineChart.getData().remove(CurrentSeriesList.get(index));
-                }
-            };
-            countryBoxes.get(i).setOnAction(checkBoxEventHandler);
-        }
+        addDataSeries(dataType, lineChart, CurrentSeriesList, holder, countryBoxes, 1);
 
         
         this.chart.getChildren().addAll(lineChart, dataType);
@@ -137,10 +132,28 @@ public class ChartGenerator  {
 
 
     }
-    public VBox getChart(){
+    public HBox getChart(){
         return chart;
     }
-
+    
+    public HBox getBarChart(){
+        return barChart;
+    }
+    public List<XYChart.Series<String, Number>> stringGDPseries() {
+        List<XYChart.Series<String, Number>> stringGDPSeriesList = new ArrayList<>();
+        for (int i = 0; i < 31; i++){
+            XYChart.Series<String, Number> GDPdata = new XYChart.Series<>();
+            GDPdata.setName(scraper.getData().get(i*8).getCountry());
+            
+            for (int x = (i * 8); x < ((i+1) * 8); x++){
+                GDPdata.getData().add(new XYChart.Data<>(Integer.toString(scraper.getData().get(x).getYear()), scraper.getData().get(x).getGDP()));
+                }
+            
+            
+            stringGDPSeriesList.add(GDPdata);    
+        }
+        return stringGDPSeriesList;
+    }
     public List<XYChart.Series<Number, Number>> GDPseries() {
         List<XYChart.Series<Number, Number>> GDPSeriesList = new ArrayList<>();
         for (int i = 0; i < 31; i++){
@@ -156,21 +169,56 @@ public class ChartGenerator  {
         }
         return GDPSeriesList;
     }
-
-    public List<XYChart.Series<Number, Number>> personnelSeries() {
-        List<XYChart.Series<Number, Number>> personnelSeriesList = new ArrayList<>();
+    public List<XYChart.Series<String, Number>> stringPersonnelSeries() {
+        List<XYChart.Series<String, Number>> stringPersonnelSeriesList = new ArrayList<>();
         for (int i = 0; i < 31; i++){
-            XYChart.Series<Number, Number> personnelData = new XYChart.Series<>();
-            personnelData.setName(scraper.getData().get(i*8).getCountry());
-            
+            XYChart.Series<String, Number> stringPersonnelData = new XYChart.Series<>();
+
+            stringPersonnelData.setName(scraper.getData().get(i*8).getCountry());
             for (int x = (i * 8); x < ((i+1) * 8); x++){
-                personnelData.getData().add(new XYChart.Data<>(scraper.getData().get(x).getYear(), scraper.getData().get(x).getPersonnel()));
+                
+
+                    stringPersonnelData.getData().add(new XYChart.Data<>(Integer.toString(scraper.getData().get(x).getYear()), scraper.getData().get(x).getPersonnel()));
                 }
             
             
+            stringPersonnelSeriesList.add(stringPersonnelData);    
+    }
+    return stringPersonnelSeriesList;
+    }
+
+
+
+    public List<XYChart.Series<Number, Number>> personnelSeries() {
+        List<XYChart.Series<Number, Number>> personnelSeriesList = new ArrayList<>();
+        
+        for (int i = 0; i < 31; i++){
+            XYChart.Series<Number, Number> personnelData = new XYChart.Series<>();
+            personnelData.setName(scraper.getData().get(i*8).getCountry());
+            for (int x = (i * 8); x < ((i+1) * 8); x++){
+                personnelData.getData().add(new XYChart.Data<>(scraper.getData().get(x).getYear(), scraper.getData().get(x).getPersonnel()));
+            }
             personnelSeriesList.add(personnelData);    
     }
     return personnelSeriesList;
+    }
+
+    public List<XYChart.Series<String,Number>> stringBudgetSeries(){
+        List<XYChart.Series<String, Number>> stringBudgetSeriesList = new ArrayList<>();
+        for (int i = 0; i < 31; i++){
+            XYChart.Series<String, Number> budgetData = new XYChart.Series<>();
+            budgetData.setName(scraper.getData().get(i*8).getCountry());
+                for (int x = (i * 8); x < ((i+1) * 8); x++){
+                  
+                    budgetData.getData().add(new XYChart.Data<>(Integer.toString(scraper.getData().get(x).getYear()), scraper.getData().get(x).getBudget()));
+                }
+                stringBudgetSeriesList.add(budgetData);   
+
+                }
+    
+            return stringBudgetSeriesList;
+       
+
     }
 
     public List<XYChart.Series<Number, Number>> budgetSeries() {
@@ -178,18 +226,105 @@ public class ChartGenerator  {
         for (int i = 0; i < 31; i++){
             XYChart.Series<Number, Number> budgetData = new XYChart.Series<>();
             budgetData.setName(scraper.getData().get(i*8).getCountry());
-            
-            for (int x = (i * 8); x < ((i+1) * 8); x++){
-                budgetData.getData().add(new XYChart.Data<>(scraper.getData().get(x).getYear(), scraper.getData().get(x).getBudget()));
+                for (int x = (i * 8); x < ((i+1) * 8); x++){
+
+                    budgetData.getData().add(new XYChart.Data<>(scraper.getData().get(x).getYear(), scraper.getData().get(x).getBudget()));
                 }
-            
-            
-            budgetSeriesList.add(budgetData);    
-        }
-    return budgetSeriesList;
+                budgetSeriesList.add(budgetData);   
+                }
+    
+            return budgetSeriesList;
     }
 
 
+    public void addDataSeries(ChoiceBox n, XYChart chart, List<XYChart.Series<Number, Number>> a, List<XYChart.Series<String, Number>> b, List<CheckBox> checkBoxSet, int j){
+        
+        n.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if(newValue.equals("Personnel")){
+                chart.setTitle("Personnel");
+                chart.getData().clear();
+                a.clear();
+                b.clear();
+
+                for (int i = 0; i < 31; i++){
+                    checkBoxSet.get(i).setSelected(false);
+                    if (j == 1){
+                        a.add(this.personnelSeries().get(i));
+                    }
+                    else{
+                        b.add(this.stringPersonnelSeries().get(i));
+                    }
+                    
+                }
+            }
+
+            else if (newValue.equals("GDP")){
+                chart.setTitle("GDP");
+                chart.getData().clear();
+                a.clear();
+                b.clear();
+
+
+                for (int i = 0; i < 31; i++){
+                    checkBoxSet.get(i).setSelected(false);
+                    if (j == 1){
+                        a.add(this.GDPseries().get(i));
+                    }
+                    else{
+                        b.add(this.stringGDPseries().get(i));
+                    }
+            
+                    }
+
+                }
+            
+            else if (newValue.equals("Military Spending")){
+                
+                chart.setTitle("Military Spending");
+
+                chart.getData().clear();
+                a.clear();
+                b.clear();
+
+
+                for (int i = 0; i < 31; i++){
+                    checkBoxSet.get(i).setSelected(false);
+                    if (j == 1){
+                        a.add(this.budgetSeries().get(i));
+                    }
+                    else{
+                        b.add(this.stringBudgetSeries().get(i));
+                    }
+             
+            }
+        }
+        });
+
+        for (int i = 0; i < 31; i++){
+            final int index = i; 
+            EventHandler<ActionEvent> checkBoxEventHandler = 
+            e -> {
+                if(checkBoxSet.get(index).isSelected()){
+                    if (j == 1){
+                        chart.getData().add(a.get(index));
+                    }
+                    else{
+                        chart.getData().add(b.get(index));
+                    }
+                    
+                }
+                else{
+                    if (j == 1){
+                        chart.getData().remove(a.get(index));
+                    }
+                    else{
+                        chart.getData().remove(b.get(index));
+                    }
+                }
+            };
+            checkBoxSet.get(i).setOnAction(checkBoxEventHandler);
+        }
+    }
 }
 
 
